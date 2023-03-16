ca4845ebbaeff90b6bb667fbda8c2ff0790986c7
Sougandhika T N <SNMT720@MGTECHSOFT.IN>
2023-03-16 12:40:16 +0530

    add.Docker_Jenkins_Sample_Project

---
+++ src/main/java/Docker_Jenkins_Project/Docker_Jankins_Sample_Project/App.java
@@ -0,0 +1,13 @@
+package Docker_Jenkins_Project.Docker_Jankins_Sample_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
